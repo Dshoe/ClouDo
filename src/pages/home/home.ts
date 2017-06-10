57ea07f25f6cee61711446c32ca13bf12b7924d9
Devin Shoemaker <devinshoe@gmail.com>
2017-06-10 17:13:45 -0500

    home: implement TasksProvider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 import {TasksProvider} from "../../providers/tasks/tasks";
 
 @Component({
@@ -8,8 +8,79 @@ import {TasksProvider} from "../../providers/tasks/tasks";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public taskService: TasksProvider) {
+  tasks: any;
 
+  constructor(public navCtrl: NavController, public tasksService: TasksProvider, public alertCtrl: AlertController) {
+
+  }
+
+  ionViewDidLoad(){
+
+    this.tasksService.getTasks().then((data) => {
+      this.tasks = data;
+    });
+
+  }
+
+  createTask(){
+
+    let prompt = this.alertCtrl.create({
+      title: 'Add',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.tasksService.createTask({title: data.title});
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+
+  }
+
+  updateTask(todo){
+
+    let prompt = this.alertCtrl.create({
+      title: 'Edit',
+      message: 'Change your mind?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.tasksService.updateTask({
+              _id: todo._id,
+              _rev: todo._rev,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  deleteTask(todo){
+    this.tasksService.deleteTask(todo);
   }
 
 }
