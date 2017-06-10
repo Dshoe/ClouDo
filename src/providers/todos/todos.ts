4d1a4a34b6d04342bd29abbba7aa91b6330a1f33
Devin Shoemaker <devinshoe@gmail.com>
2017-06-10 12:24:00 -0500

    getTodos() This function will store and return todos from the local database. As it returns the data it checks to see if any changes have been made to the database since the last time it was stored locally.

--- src/providers/todos/todos.ts
+++ src/providers/todos/todos.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TodosProvider {
 
+  data: any;
   db: any;
   remote: any;
 
@@ -20,6 +21,57 @@ export class TodosProvider {
     };
 
     this.db.sync(this.remote, options);
+  }
+
+  getTodos() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.db.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({live: true, since: 'now', include_docs:true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      })
+    })
+  }
+
+  handleChange(change) {
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+    });
+
+    // A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    } else {
+      // A document was updated or added
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      } else {
+        this.data.push(change.doc);
+      }
+
+    }
   }
 
 }
